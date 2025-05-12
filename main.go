caff05c82b32c71c1079094bb329a27dc54d0acc
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-05-12 18:22:26 +0900

    feat

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool() *pgxpool.Pool {
+	connStr := getConnStr()
+	ctx := context.Background()
+
+	db, err := pgxpool.New(ctx, connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+
+	return db
+}
+
+func getConnStr() string {
+	user := getEnv("DB_USER", "admin")
+	pass := getEnv("DB_PASS", "admin")
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "5432")
+	dbname := getEnv("DB_NAME", "postgres")
+
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, pass, host, port, dbname)
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
 
 func main() {
+	conn := NewPool()
+	defer conn.Close()
 	fmt.Print("Hello World")
 }
