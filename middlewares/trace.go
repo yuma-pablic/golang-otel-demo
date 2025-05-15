b9fac160c0c304185d891d5bfbb4790814434e35
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-05-16 00:44:12 +0900

    feat

--- middlewares/trace.go
+++ middlewares/trace.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"otel/ctxx"
@@ -12,16 +11,13 @@ import (
 func TraceIDMiddleware(tracer trace.Tracer) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			traceID := GenerateTraceID(r.Context(), tracer, r.Method, r.URL.Path)
-			ctx := ctxx.SetTraceID(r.Context(), traceID)
+			ctx, span := tracer.Start(r.Context(), fmt.Sprintf("%s %s", r.Method, r.URL.Path))
+			defer span.End()
+
+			traceID := span.SpanContext().TraceID().String()
+			ctx = ctxx.SetTraceID(ctx, traceID)
+
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
-}
-
-func GenerateTraceID(ctx context.Context, tracer trace.Tracer, method, path string) string {
-	_, span := tracer.Start(ctx, fmt.Sprintf("%s %s", method, path))
-	defer span.End()
-
-	return span.SpanContext().TraceID().String()
 }
