0f9bd6a121b4e922a696eaabae47603f27477d76
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-05-26 00:24:52 +0900

    feat

--- middlewares/metrics.go
+++ middlewares/metrics.go
@@ -1,8 +1,6 @@
 package middlewares
 
 import (
-	"context"
-	"math/rand"
 	"net/http"
 	"time"
 
@@ -18,8 +16,6 @@ func MetricsMiddleware(tracer trace.Tracer, histogram metric.Float64Histogram) f
 			defer span.End()
 
 			startTime := time.Now()
-			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-			prosessing(ctx, tracer)
 
 			duration := time.Since(startTime)
 			histogram.Record(
@@ -30,27 +26,4 @@ func MetricsMiddleware(tracer trace.Tracer, histogram metric.Float64Histogram) f
 
 		})
 	}
-}
-
-func prosessing(ctx context.Context, tracer trace.Tracer) {
-	ctx, span := tracer.Start(ctx, "processing...")
-	defer span.End()
-
-	if rand.Float64() < 1.0/100.0 {
-		funcAbnormal(ctx, tracer)
-	} else {
-		funcNormal(ctx, tracer)
-	}
-}
-
-func funcNormal(ctx context.Context, tracer trace.Tracer) {
-	_, span := tracer.Start(ctx, "funcNormal")
-	defer span.End()
-	time.Sleep(10 * time.Millisecond)
-}
-
-func funcAbnormal(ctx context.Context, tracer trace.Tracer) {
-	_, span := tracer.Start(ctx, "funcAbNormal(Oh...taking a lot of time...)")
-	defer span.End()
-	time.Sleep(3 * time.Second)
 }
